6958065735ab69a0ec1f21ded17e7f6a99a2e477
Nemanja Broceta <brocetanemanja@gmail.com>
2020-10-12 14:52:10 +0200

    Rework of firstBranch

--- drupal/modules/custom/drupalup_controller/src/Controller/ArticleController.php
+++ drupal/modules/custom/drupalup_controller/src/Controller/ArticleController.php
@@ -9,18 +9,18 @@ class ArticleController {
   public function page() {
 
     $db = Database::getConnection();
-    $query = $db->select('node_revision__field_title', 'nrft');
-    $query->join('node_revision__field_desciption', 'nrfd', 'nrfd.entity_id = nrft.entity_id');
-    $query->join('node_revision__field_picture', 'nrfp', 'nrfd.entity_id = nrfp.entity_id');
-    $query->fields('nrft', array('field_title_value'));
-    $query->fields('nrfd', array('field_desciption_value'));
-    $query->fields('nrfp', array('field_picture_alt'));
+    $query = $db->select('node__field_title', 'nrft');
+    $query->join('node__field_desciption', 'nrfd', 'nrfd.entity_id = nrft.entity_id');
+    $query->join('node__field_picture', 'nrfp', 'nrfd.entity_id = nrfp.entity_id');
+    $query->fields('nrft', ['field_title_value']);
+    $query->fields('nrfd', ['field_desciption_value']);
+    $query->fields('nrfp', ['field_picture_alt']);
     $result = $query->execute()->fetchAll();
 
-    return array(
+    return [
       '#theme' => 'article_list',
       '#records' => $result,
       '#title' => 'Our article list'
-    );
+    ];
   }
 }
